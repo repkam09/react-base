64967a895b6565f298f3ee10875de22dbdf14671
Mark Repka <mark@repkam09.com>
2017-10-13 16:05:34 -0400

    Switch to create-react-app base

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-const ExampleComponent = require("./react/example.jsx");
-ReactDOM.render(<ExampleComponent />, document.getElementById('example'));
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
